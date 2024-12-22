39a09c6fe7477a87f35c1db87c8300d34190d05c
xiaozheng <zyx52875287@163.com>
2024-12-23 06:49:51 +0800

    引用element-plus

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <div class="common-layout"> -->
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+      <el-aside width="200px">Aside</el-aside>
+    </el-container>
+  <!-- </div> -->
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
