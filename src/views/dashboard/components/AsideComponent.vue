68f9178734174e29e7e1e09bdbcb7def55b6618a
xiaozheng <zyx52875287@163.com>
2024-12-25 07:01:02 +0800

    更换draggable版本

--- src/views/dashboard/components/AsideComponent.vue
+++ src/views/dashboard/components/AsideComponent.vue
@@ -1,31 +1,59 @@
 <template>
   <div v-for="field in fieldsList" :key="field.title" class="item">
-    <draggable v-model="field.list">
-      <template v-for="(item, cIndex) in field.list"
-          :key="'c_' + cIndex">
-        <div
+  <!-- <template v-if="field.list.find((f) => includeFieldsType.includes(f.type))"> -->
+    <div class="field-title">{{ field.title }}</div>
+    <Draggable
+      tag="ul"
+      :list="field.list"
+      :group="{ name: 'form', clone: (original) => ({ ...original }), putTo: false }"
+      :sort="false"
+      drag-class="ghost"
+    >
+      <template #item="{ element, index }">
+        <li
           class="field-label"
+          v-if="includeFieldsType.includes(element.type)"
+          :key="'c_' + index"
         >
-          <a @click="handleFieldClick(item)">
-            <i class="icon iconfont" :class="item.icon"></i>
-            <span>{{ item.title || item.label }}</span>
+          <a @click="handleFieldClick(element)">
+            <i class="icon iconfont" :class="element.icon"></i>
+            <span>{{ element.title || element.label }}</span>
           </a>
-        </div>
+        </li>
       </template>
-    </draggable>
-  </div>
+    </Draggable>
+  <!-- </template> -->
+</div>
 </template>
 
 <script setup>
-import draggable from "vuedraggable";
+import Draggable from "vuedraggable";
 import fieldsList from "../fieldsConfig";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 const handleFieldClick = (item) => {
   console.log(item);
-}
-watch(() => fieldsList, (newVal) => {
-  console.log(newVal)
-}, { deep: true, immediate: true })
+};
+watch(
+  () => fieldsList,
+  (newVal) => {
+    console.log(newVal);
+  },
+  { deep: true, immediate: true }
+);
+
+const includeFieldsType = computed(() => {
+  const arr = [];
+  console.log("fields", fieldsList);
+
+  fieldsList.forEach((f) => {
+    f.list.forEach((c) => {
+      arr.push(c.type);
+    });
+  });
+  console.log("arr", arr);
+
+  return arr;
+});
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div class="common-layout"> -->
     <el-container>
-      <el-aside width="200px">
+      <el-aside width="400px">
         <AsideComponent />
       </el-aside>
       <el-main>Main</el-main>
